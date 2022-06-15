50a81578bbf091a39119e2dc40625b91bb0879f6
dsp755 <people13@ya.ru>
2022-06-15 18:14:11 +0300

    add lists creation and deletion

--- index.js
+++ index.js
@@ -6,13 +6,7 @@ dotenv.config()
 
 const TOKEN = process.env.TOKEN;
 const bot = new telegramBot(TOKEN, { polling: true });
-const db_path = './db.json'
-
-const categories = {
-  'дела': 'tasks',
-  'продукты': 'food',
-  'фильмы': 'movies'
-}
+const db_path = '../DB/bot_pulling_db/bot_pulling_db.json'
 
 const createList = (array) => {
   let list = ''
@@ -20,16 +14,16 @@ const createList = (array) => {
   return list
 }
 
-const showCategory = (chat_id, text, db) => {
-  const list = createList(db[categories[text.toLowerCase()]])
+const showList = (chat_id, text, db) => {
+  const list = createList(db[text.toLowerCase()])
   if (list.length) {
-    bot.sendMessage(chat_id, createList(db[categories[text.toLowerCase()]]))
+    bot.sendMessage(chat_id, createList(db[text.toLowerCase()]))
   } else {
-    bot.sendMessage(chat_id, `В категории "${text}" нет записей.`)
+    bot.sendMessage(chat_id, `В списке "${text}" нет записей.`)
   }
 }
 
-const modifiedData = (type, db, text) => {
+const action = (type, db, text) => {
   let trimmedText;
   let removedItem;
   if (type === 'add') {
@@ -38,35 +32,34 @@ const modifiedData = (type, db, text) => {
   if (type === 'remove') {
     trimmedText = text.replace(`удалить `, '')
   }
-  const resultCategory = { category: '', matchLetters: 0 }
-  Object.keys(categories).forEach(category => {
+  const resultList = { list: '', matchLetters: 0 }
+  Object.keys(db).forEach(dbList => {
     let matchLetters = 0;
-    category.split('').forEach(catLetter => {
-      trimmedText.split('').forEach(textLetter => {
-        if (textLetter === catLetter) {
+    dbList.split('').forEach(listLetter => {
+      trimmedText.split(' ')[0].split('').forEach(textLetter => {
+        if (textLetter === listLetter) {
           matchLetters += 1;
         }
       })
     })
-    if (matchLetters > resultCategory.matchLetters) {
-      resultCategory.category = category
-      resultCategory.matchLetters = matchLetters
+    if (matchLetters > resultList.matchLetters) {
+      resultList.list = dbList
+      resultList.matchLetters = matchLetters
     }
   })
-  const rusCategory = resultCategory.category
   const newTrimmedText = trimmedText.split(' ').splice(1).join(' ')
   console.log('newTrimmedText: ', newTrimmedText);
   
   if (type === 'add') {
-    db[categories[rusCategory]] = db[categories[rusCategory]].concat(newTrimmedText)
+    db[resultList.list] = db[resultList.list].concat(newTrimmedText)
   }
   if (type === 'remove') {
-    removedItem = db[categories[rusCategory]]
+    removedItem = db[resultList.list]
       .find(item => item.includes(newTrimmedText.split()[newTrimmedText.split().length - 1]))
-    db[categories[rusCategory]] = db[categories[rusCategory]]
+    db[resultList.list] = db[resultList.list]
       .filter(item => !item.includes(newTrimmedText.split()[newTrimmedText.split().length - 1]))
   }
-  return { newDb: db, category: rusCategory, item: newTrimmedText, removedItem }
+  return { newDb: db, list: resultList.list, item: newTrimmedText, removedItem }
 }
 
 bot.on("message", async (message) => {
@@ -78,36 +71,76 @@ bot.on("message", async (message) => {
     const db = JSON.parse(dbData)
     const text = message.text.toLowerCase()
 
+    // SHOW ALL LISTS
+    if (text.includes('списки')) {
+      const noLists = 'Нет активных списков. Пример сообщения для добавления списка: "добавить список фильмы".'
+      bot.sendMessage(chat_id, createList(Object.keys(db)) || noLists)
+      return;
+    }
+
+    // CREATE A NEW LIST
+    if (text.includes('добавить список')) {
+      const listName = text.split(' ').splice(2).join(' ').trim()
+      const dbWithNewList = { ...db, [listName]: [] }
+      writeFile(db_path, JSON.stringify(dbWithNewList), (error) => {
+        if (error) {
+          console.log('An error has occurred ', error);
+          return;
+        }
+      });
+      bot.sendMessage(chat_id, `Добавлен новый список "${listName}".`)
+      return;
+    }
+
+    // DELETE A LIST
+    if (text.includes('удалить список')) {
+      const listName = text.split(' ').splice(2).join(' ').trim()
+      if (!Object.keys(db).includes(listName)) {
+        bot.sendMessage(chat_id, `Список "${listName}" не найден.`)
+        return;
+      }
+      const dbCopy = { ...db }
+      delete dbCopy[listName]
+      writeFile(db_path, JSON.stringify(dbCopy), (error) => {
+        if (error) {
+          console.log('An error has occurred ', error);
+          return;
+        }
+      });
+      bot.sendMessage(chat_id, `Список "${listName}" удален.`)
+      return;
+    }
+
     // SHOW LIST
     // If message text is one word
     if (text.split(' ').length === 1) {
-      showCategory(chat_id, text, db)
+      showList(chat_id, text, db)
     }
 
     // ADD ITEM TO A LIST
     if (text.includes('добавить')) {    
-      const { newDb, category, item } = modifiedData('add', db, text)
-      await bot.sendMessage(chat_id, `"${item}" добавлено в категорию ${category}`)
+      const { newDb, list, item } = action('add', db, text)
+      await bot.sendMessage(chat_id, `"${item}" добавлено в список ${list}`)
       writeFile(db_path, JSON.stringify(newDb), (error) => {
         if (error) {
           console.log('An error has occurred ', error);
           return;
         }
       });
-      showCategory(chat_id, category, newDb)
+      showList(chat_id, list, newDb)
     }
 
     // REMOVE ITEM FROM A LIST
     if (text.includes('удалить')) {    
-      const { newDb, category, item, removedItem } = modifiedData('remove', db, text)
-      await bot.sendMessage(chat_id, `"${removedItem}" удалено из категории ${category}`)
+      const { newDb, list, item, removedItem } = action('remove', db, text)
+      await bot.sendMessage(chat_id, `"${removedItem}" удалено из списка ${list}`)
       writeFile(db_path, JSON.stringify(newDb), (error) => {
         if (error) {
           console.log('An error has occurred ', error);
           return;
         }
       });
-      showCategory(chat_id, category, newDb)
+      showList(chat_id, list, newDb)
     }
   }
 });
